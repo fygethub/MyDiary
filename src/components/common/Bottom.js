8bdcf0cc9320883b5a4bdad64d3015e8c498ec18
fygethub <13658399878@163.com>
2017-03-19 16:30:09 +0800

    add Header Bottom Component 😯

---
+++ src/components/common/Bottom.js
@@ -0,0 +1,69 @@
+import  React, { Component } from 'react';
+import pureRender from 'pureRender';
+import renderLog from 'renderLog';
+
+import bottomStyle from 'style/bottom.css';
+import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+import SvgIcon from 'material-ui/SvgIcon';
+import { grey500, greenA200} from 'material-ui/styles/colors';
+import { Link } from 'react-router-dom';
+console.log(bottomStyle)
+
+const HomeIcon = (props) => (
+    <SvgIcon {...props}>
+        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+);
+const HeartIcon = (props) => (
+    <SvgIcon {...props}>
+        <path d="M0 0h24v24H0z" fill="none"/>
+        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+    </SvgIcon>
+)
+
+@pureRender
+@renderLog
+class Bottom extends  Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedIndex: 0,
+        };
+        this.handleSelect = this.handleSelect.bind(this);
+    }
+
+    handleSelect(index) {
+        this.setState({
+            selectedIndex: index,
+        });
+    }
+
+    render(){
+        return(
+            <Paper zDepth={1} className={bottomStyle.bottom}>
+                <BottomNavigation selectedIndex={this.state.selectedIndex}>
+                    <BottomNavigationItem
+                        icon={<Link to="/content"><HomeIcon color={this.state.selectedIndex === 0 ? greenA200 : grey500 }/></Link> }
+                        onTouchTap={() => this.handleSelect(0)}
+                        label={ <Link to="/content">Home</Link> }
+                    />
+                    <BottomNavigationItem
+                        icon={<Link to="/lovelist"><HeartIcon color={this.state.selectedIndex === 1 ? greenA200 : grey500 }/></Link> }
+                        onTouchTap={() => this.handleSelect(1)}
+                        label={ <Link to="/lovelist">Favorite</Link> }
+                    />
+                    <BottomNavigationItem
+                        icon={ <Link to="/location"><IconLocationOn color={this.state.selectedIndex === 2 ? greenA200 : grey500 }/></Link>}
+                        onTouchTap={()=> this.handleSelect(2) }
+                        label={ <Link to="/location">Position</Link> }
+                    />
+                </BottomNavigation>
+            </Paper>
+        )
+    }
+
+}
+
+export default Bottom;
---
+++ src/components/common/Header.js
@@ -0,0 +1,73 @@
+import React ,{  Component } from 'react';
+import styles from "../../style/header.css"
+
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import FlatButton from 'material-ui/FlatButton';
+import SvgIcon from 'material-ui/SvgIcon';
+import renderLog from  'renderLog';
+import pureRender from 'pureRender';
+
+console.log(styles)
+const _styles = {
+    title: {
+        cursor: 'pointer',
+        textAlign: 'center',
+        position: 'absolute',
+        top: '0',
+    },
+    headerRight: {
+        margin: '0 !important',
+    },
+    fontSizeBase: {
+        fontSize: '12px !important',
+    }
+
+};
+
+const MenuList = (props) => (
+    <SvgIcon {...props} >
+        <path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"/>
+        <path d="M0 0h24v24H0z" fill="none"/>
+    </SvgIcon>
+)
+
+const HeaderRightMenu = (props) => (
+    <span {...props}>save</span>
+)
+
+@pureRender
+@renderLog
+class  Header extends  Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            index:1
+        }
+        this.handleTouchTap = this.handleTouchTap.bind(this);
+    }
+
+     handleTouchTap() {
+         console.log('tap !');
+    }
+
+    render() {
+        let { match } = this.props;
+        match = match || '日记本'
+        console.log(match);
+        return (
+            <AppBar
+                className={styles.header}
+                title={ match.id }
+                onTitleTouchTap={this.handleTouchTap}
+                iconElementLeft={<IconButton><MenuList /></IconButton>}
+                iconElementRight={<FlatButton label={<HeaderRightMenu style={_styles.fontSizeBase }/>} style={ _styles.headerRight } />}
+                iconStyleLeft={{marginLeft: '-5px', marginRight: '36px'}}
+            />
+        )
+
+    }
+}
+
+export  default Header;
