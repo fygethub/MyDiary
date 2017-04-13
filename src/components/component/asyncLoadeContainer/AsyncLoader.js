38f9e64b82f9bdf2fe99020866b78e6b3b003557
fygethub <13658399878@163.com>
2017-04-13 16:39:51 +0800

    update Asy

--- src/components/component/asyncLoadeContainer/AsyncLoader.js
+++ src/components/component/asyncLoadeContainer/AsyncLoader.js
@@ -7,7 +7,7 @@ import Posts from '../asyncLoade/Posts'
 class AsyncLoader extends Component{
     static propTypes = {
         selectedReddit: PropTypes.string.isRequired,
-        Posts: PropTypes.array.isRequired,
+        posts: PropTypes.array.isRequired,
         isFetching: PropTypes.bool.isRequired,
         lastUpdated: PropTypes.number,
         dispatch: PropTypes.func.isRequired,
@@ -31,11 +31,74 @@ class AsyncLoader extends Component{
         this.props.dispatch(selectReddit(nextReddit))
     }
 
+    handleRefreshClick = e => {
+        e.preventDefault();
+
+        const { dispatch, selectedReddit } = this.props;
+        dispatch(invalidateReddit(selectedReddit))
+        dispatch(fetchPostsIfNeeded(selectedReddit))
+    }
+
+    render() {
+        const { selectedReddit, posts, isFetching, lastUpdated } = this.props;
+        const isEmpty = posts.length === 0;
+        return (
+            <div>
+                <Picker value={selectedReddit}
+                        onChange={this.handleChange}
+                        options={['reactjs','frontend']}
+                />
+                <p>
+                    {
+                        lastUpdated &&
+                            <span>
+                               Last update at { new Date(lastUpdated).toLocaleTimeString()}
+                                {' '}
+                               </span>
+                    }
+                    {
+                        !isFetching &&
+                        <a href="#"
+                           onClick={this.handleRefreshClick}
+                        >
+                            Refresh
+                        </a>
+                    }
+                </p>
+                {
+                    isEmpty ?
+                        (isFetching ? <h2>Loading ... </h2> : <h2>Empty.</h2>)
+                        : <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+                            <Posts posts={posts} />
+                        </div>
+                }
+            </div>
+        )
+    }
 }
 
+const mapStateToProps = state => {
+    const { selectedReddit, postsByReddit } = state;
+    const {
+        isFetching,
+        lastUpdated,
+        items: posts,
+    } = postsByReddit[selectedReddit] || {
+        isFetching: true,
+        items: []
+    }
 
+    console.log(posts);
 
+    return {
+        selectedReddit,
+        posts,
+        isFetching,
+        lastUpdated
+    }
+}
 
+export default connect(mapStateToProps)(AsyncLoader)
 
 
 
